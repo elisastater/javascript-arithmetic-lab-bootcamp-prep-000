da69b3d05b23a1d998bd8d29e3cdb7bd47a9932b
elisa stater <elisa_60@msn.com>
2018-10-09 17:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000);
 });
 
-function makeInt(a, b) {
+function makeInt(0, b) {
 	var parsed = (2328, b);
 	if (isNaN(parsed)) {return 0}
     {return parsed * 0}
