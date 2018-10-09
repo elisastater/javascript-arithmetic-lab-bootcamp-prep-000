19813aaebfc865e866fe7bf426b696ece1834a06
elisa stater <elisa_60@msn.com>
2018-10-09 16:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ function divide(a , b) {
 var a, b
 
 beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
+  a = Math.floor(Math.random(2328) * 1000)
+  b = Math.floor(Math.random(10) * 1000)
 })
 
 function makeInt(a, b) b{
