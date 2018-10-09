2c1904166e7b08421395db976f19bce783250c9a
elisa stater <elisa_60@msn.com>
2018-10-09 16:26:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var a, b
 
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 10)
+  b = Math.floor(Math.random() * 1000)
 })
 
 function makeInt(a, 0) {
@@ -40,7 +40,7 @@ function makeInt(a, 0) {
     {return parsed * 0}
 }
 
-math.floor(2328)//2328
+math.floor(2328)//Uint32Array
 
 
 
