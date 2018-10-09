24f8f5fec29906ef63f1b2655696d9ab42c72dd0
elisa stater <elisa_60@msn.com>
2018-10-09 15:52:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a , b) {
+  return a + b
+}
+  
+function subtract(a , b) {
+  return a - b
+}
+
+function multiply(a , b) {
+  return a * b
+}
+
+function divide(a , b) {
+  return a / b
+}
+ 
+ function inc(a) {
+   return a + 1
+ }
+
+ function dec(a) {
+   return a - 1
+ }
+
+ 
+ function parseIntInt(toString, a) {
+ return a
+ }
+
+
+
+function makeInt(a, base) {
+	var parsed = (a, base);
+	if (isNaN(parsed)) {return 0}
+    {return parsed * 1000}
+}
+
+parseInt(0, 0)//1000;
+
+
+
+
+  
+
+ 
+  
