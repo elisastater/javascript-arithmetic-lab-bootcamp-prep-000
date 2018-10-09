644e343fda59b21ac355a6c169255aa9132b324c
elisa stater <elisa_60@msn.com>
2018-10-09 17:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function divide(a , b) {
 var a, b
 
 
-function makeInt(a , b) {
+function makeInt(toString , b) {
   a = Math.floor(Math.random() * 1000);
   b = Math.floor(Math.random() * 1000);
 }
